ff0da009d65cc797073b0400552f97ce6e8fab70
Adam S. Crane <adam.crane@sas.com>
2014-01-24 17:02:09 -0500

    script for installing pyenv with virtualenvwrapper plugin

---
+++ install-pyenv-virtualenvwrapper.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+installdir = $(pwd)
+git clone git://github.com/yyuu/pyenv.git .pyenv
+cd .pyenv/plugins
+git clone https://github.com/yyuu/pyenv-virtualenvwrapper.git
+
+# bootstrap 
+export PATH="$installdir/bin:$PATH"
+export PYENV_ROOT="$installdir"
+eval "$(pyenv init -)"
+pyenv virtualenvwrapper
+
+# add to profile
+echo "export PATH=\"$installdir/bin:$PATH\"" >> ~/.bash_profile
+echo <<'EOF' >> ~/.bash_profile
+if which pyenv > /dev/null; then
+    export PYENV_ROOT="$(pyenv root)/.pyenv"
+    eval "$(pyenv init -)"
+    pyenv virtualenvwrapper
+fi
+EOF

e39ae7659a7c400b1431e58203f6e7c1ceea8b31
Adam S. Crane <Adam.Crane@sas.com>
2014-01-27 16:01:11 -0500

    added homebrew flag, tokenization to variables to pyenv setup

--- install-pyenv-virtualenvwrapper.sh
+++ install-pyenv-virtualenvwrapper.sh
@@ -1,22 +1,44 @@
 #!/bin/bash
+set -ue
 
-installdir = $(pwd)
-git clone git://github.com/yyuu/pyenv.git .pyenv
-cd .pyenv/plugins
-git clone https://github.com/yyuu/pyenv-virtualenvwrapper.git
+shellprofile="~/.bash_profile"
+installdir=$(pwd)
+pyenvsubdir=".pyenv"
+virtualenvsubdir=".virtualenvs"
+projectsubdir=""
+uselazyloading=true
+usehomebrew=false
+
+if $uselazyloading; then
+    wrapperinit="virtualenvwrapper_lazy"
+else
+    wrapperinit="virtualenvwrapper"
+fi
+
+if $usehomebrew; then
+    brew install pyenv pyenv-virtualenv
+else
+    git clone git://github.com/yyuu/pyenv.git "$pyenvsubdir"
+    git clone https://github.com/yyuu/pyenv-virtualenv.git "$pyenvsubdir/plugins/pyenv-virtualenv"
+fi
+git clone https://github.com/yyuu/pyenv-virtualenvwrapper.git "$pyenvsubdir/plugins/pyenv-virtualenvwrapper"
 
 # bootstrap 
-export PATH="$installdir/bin:$PATH"
+$usehomebrew || export PATH="$installdir/$pyenvsubdir/bin:$PATH"
 export PYENV_ROOT="$installdir"
 eval "$(pyenv init -)"
-pyenv virtualenvwrapper_lazy
+pyenv $wrapperinit
 
-# add to profile
-echo "export PATH=\"$installdir/bin:$PATH\"" >> ~/.bash_profile
-echo <<'EOF' >> ~/.bash_profile
+# add to shell profile
+echo "export WORKON_HOME=\"${installdir}/${virtualenvsubdir}\" #virtualenvs created here" >> "$shellprofile"
+if [ "$projectsubdir" -ne "" ]; then
+    echo "export PROJECT_HOME=\"${installdir}/${projectsubdir}\" #projects created here" >> "$shellprofile"
+fi
+$usehomebrew || echo "export PATH=\"${installdir}/${pyenvsubdir}/bin:\$PATH\"" >> "$shellprofile"
+echo <<EOF >> ~/.bash_profile
+# initialize pyenv
 if which pyenv > /dev/null; then
-    export PYENV_ROOT="$(pyenv root)/.pyenv"
-    eval "$(pyenv init -)"
-    pyenv virtualenvwrapper_lazy
+    eval "\$(pyenv init -)"
+    pyenv $wrapperinit
 fi
 EOF

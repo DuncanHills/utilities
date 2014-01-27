2097846941314afa1b4aa20584e1446907aaf7f5
Adam S. Crane <Adam.Crane@sas.com>
2014-01-27 17:27:39 -0500

    added gitclobber() function to pyenv installer

--- install-pyenv-virtualenvwrapper.sh
+++ install-pyenv-virtualenvwrapper.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 set -ue
 
-shellprofile="~/.bash_profile"
+shellprofile="${HOME}/.bash_profile"
 installdir=$(pwd)
 pyenvsubdir=".pyenv"
 virtualenvsubdir=".virtualenvs"
 projectsubdir=""
 uselazyloading=true
 usehomebrew=false
+
+gitclobber() {
+    if [ -z "$1" -o -z "$2" ]; then
+        echo "Wrong number of arguments to gitclobber()" &>2
+    fi
+
+    rm -rf "${2}/.git"
+    git clone --no-checkout "$1" .gitclobber
+    mv -f .gitclobber/.git "$2"
+    rm -r .gitclobber
+    git --git-dir="${2}/.git" --work-tree="$2" reset --hard HEAD
+}
 
 if $uselazyloading; then
     wrapperinit="virtualenvwrapper_lazy"
@@ -18,27 +30,25 @@ fi
 if $usehomebrew; then
     brew install pyenv pyenv-virtualenv
 else
-    git clone git://github.com/yyuu/pyenv.git "$pyenvsubdir"
-    git clone https://github.com/yyuu/pyenv-virtualenv.git "$pyenvsubdir/plugins/pyenv-virtualenv"
+    gitclobber git://github.com/yyuu/pyenv.git "$pyenvsubdir/plugins/pyenv"
+    gitclobber https://github.com/yyuu/pyenv-virtualenv.git "$pyenvsubdir/plugins/pyenv-virtualenv"
 fi
-git clone https://github.com/yyuu/pyenv-virtualenvwrapper.git "$pyenvsubdir/plugins/pyenv-virtualenvwrapper"
-
-# bootstrap 
-$usehomebrew || export PATH="$installdir/$pyenvsubdir/bin:$PATH"
-export PYENV_ROOT="$installdir"
-eval "$(pyenv init -)"
-pyenv $wrapperinit
+gitclobber https://github.com/yyuu/pyenv-virtualenvwrapper.git "$pyenvsubdir/plugins/pyenv-virtualenvwrapper"
 
 # add to shell profile
+echo "# virtualenvwrapper settings" >> "$shellprofile"
 echo "export WORKON_HOME=\"${installdir}/${virtualenvsubdir}\" #virtualenvs created here" >> "$shellprofile"
-if [ "$projectsubdir" -ne "" ]; then
+if [ "$projectsubdir" != "" ]; then
     echo "export PROJECT_HOME=\"${installdir}/${projectsubdir}\" #projects created here" >> "$shellprofile"
 fi
 $usehomebrew || echo "export PATH=\"${installdir}/${pyenvsubdir}/bin:\$PATH\"" >> "$shellprofile"
-echo <<EOF >> ~/.bash_profile
+cat >> "$shellprofile" <<EOF
 # initialize pyenv
 if which pyenv > /dev/null; then
     eval "\$(pyenv init -)"
     pyenv $wrapperinit
 fi
 EOF
+
+# restart shell
+echo "\`. ${shellprofile}\` or open a new shell session to use pyenv and virtualenvwrapper."
